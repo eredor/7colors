c64f4c2cb7b121710e665dd2b3ecd9d9df1d6388
Thibaut <thibaut.antoine@ens-rennes.fr>
2021-10-04 11:56:12 +0200

    Added the random initialization of the board

--- board.c
+++ board.c
@@ -5,13 +5,15 @@
 #include "board.h"   /* Enforce that the header file matches the declarations */
 #include "simple_unit_test.h" /* Import the testing infrastructure */
 
+#include <time.h> /* Import time to initialize random seed */
+
 /* Note: This template comes with several global definitions. For now.
- * 
- * Such globals are usually discouraged, but having a few of them is OK in a C program.
- * At first, no dinosaure will get you if you use globals, but you should seek for a better 
- * solution for your final version. 
  *
- * Encapuslating them in an appropriate data structure, and removing all globals would be a must. 
+ * Such globals are usually discouraged, but having a few of them is OK in a C program.
+ * At first, no dinosaure will get you if you use globals, but you should seek for a better
+ * solution for your final version.
+ *
+ * Encapuslating them in an appropriate data structure, and removing all globals would be a must.
  * Plus, this path often leads to simpler code, that is easier to test.
  */
 
@@ -21,6 +23,15 @@
 
 /** Represent the actual current board game */
 char board[BOARD_SIZE * BOARD_SIZE] = { 0 }; // Filled with zeros
+
+for (int i = 0; i < BOARD_SIZE; i++) {
+    for (int j = 0; j < BOARD_SIZE; j++) {
+
+        int case_idx = BOARD_SIZE * i + j;
+        srand(time + case_idx); // initialize random seed
+        board[case_idx] = (rand() % 7) + 'A';
+    }
+}
 
 /** Retrieves the color of a given board cell */
 char get_cell(int x, int y)
@@ -54,23 +65,23 @@ void print_board(void)
 /************ The tests **************/
 
 /* Tests that the initialization works */
-SUT_TEST(init_cell) 
+SUT_TEST(init_cell)
 {
    char c = get_cell(5, 5);
    SUT_CHAR_EQUAL(c, '\0', "Creating the board does not initialize the cells to '\\0' but to '%c'", c);
-   
+
    return 1;
 }
 
 /* Tests that the get_cell and set_cell work */
-SUT_TEST(getset_cell) 
+SUT_TEST(getset_cell)
 {
    char c;
-   
+
    set_cell(5, 5, 'A');
    c = get_cell(5, 5);
    SUT_CHAR_EQUAL(c, 'A', "Setting a cell to 'A' leads to '%c' as a value instead", c);
-   
+
    return 1;
 }
 
