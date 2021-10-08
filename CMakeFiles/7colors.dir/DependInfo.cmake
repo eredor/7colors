289b3958dbee3632f567b2f8e2d1604f7cf50705
Emma Redor <emma.redor@etudiant.univ-rennes1.fr>
2021-10-08 12:35:51 +0200

    Clean

---
+++ CMakeFiles/7colors.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/asus/7colors/7colors/board.c" "/home/asus/7colors/7colors/CMakeFiles/7colors.dir/board.c.o"
+  "/home/asus/7colors/7colors/main.c" "/home/asus/7colors/7colors/CMakeFiles/7colors.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/7colors.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "7colors"
+  "7colors.pdb"
+  "CMakeFiles/7colors.dir/board.c.o"
+  "CMakeFiles/7colors.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/7colors.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
